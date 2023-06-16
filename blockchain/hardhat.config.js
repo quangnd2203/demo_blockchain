116a187deeb858575904622c8183d689c4dd288f
quangnd2203 <quangnd.nta@gmail.com>
2023-06-16 14:32:35 +0700

    no message

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,5 +1,16 @@
-require('@nomicfoundation/hardhat-toolbox');
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.18",
-};
+// require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config('../.env');
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+console.log("PRV_KEY");
+console.log(process.env);
+
+// module.exports = {
+//   solidity: '0.8.18',
+//   networks: {
+//     bsctest:{
+//       url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+//       accounts: [process.env.PRIVATE_KEY]
+//     }
+//   }
+// };
