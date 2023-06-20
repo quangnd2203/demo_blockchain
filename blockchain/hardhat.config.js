45f1dfcb839256212290728abe1bd39bb98df841
quangnd2203 <quangnd.nta@gmail.com>
2023-06-20 14:33:00 +0700

    no message

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,8 +1,8 @@
 require('@nomicfoundation/hardhat-toolbox');
 require("@nomicfoundation/hardhat-verify");
 
-require('dotenv').config({ path: '../env/.env' });
-require('dotenv').config({ path: '../env/blockchain.env' });
+require('dotenv').config({ path: './.env' });
+require('dotenv').config({ path: '../.env' });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
@@ -10,7 +10,7 @@ module.exports = {
   solidity: '0.8.18',
   networks: {
     bsctest: {
-      url: process.env.NETWORK_URL,
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
       accounts: [process.env.PRIVATE_KEY]
     }
   },
--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -1,16 +1,19 @@
+const BlockchainConfig = require('./config');
+
+const { ethers } = require("hardhat");
+
+var config = new BlockchainConfig();
+
 async function main() {
     const [deployer] = await ethers.getSigners();
-
-    console.log("Deploying contracts with the account:", deployer.address);
-
-    const token = await ethers.deployContract("CucCungToken");
-
-    console.log("Token address:", await token.getAddress());
+    console.log('Deploying contracts with the account:', deployer.address);
+    const token = await ethers.deployContract('CucCungToken');
+    config.tokenAddress = await token.getAddress();
 }
 
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+main().then(() => {
+    let config = new BlockchainConfig();
+    console.log(`Token Address: ${config.tokenAddress}`);
+});
+
+module.exports = main;
